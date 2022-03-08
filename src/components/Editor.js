1e773a7b5d6e82111ef7376421b9f8ac9e33ed89
Brage <bragmin@hotmail.com>
2022-03-08 11:09:05 +0100

    renamed to Editor and added a potential savevideo function

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,16 +2,17 @@ import Draggable from "react-draggable";
 import {useState} from "react";
 const radius = 35
 
-export default function Dragger(){
+export default function Editor(){
     const [startPos, setStartPosition] = useState({ x: 0, y: 0 });
-    const [endPos, setEndPosition] = useState({ x: 0, y: 0 });
+    const [endPos, setEndPosition] = useState({ x: 200, y: 0 });
 
 
     function checkStartCollision(data) {
         setStartPosition({ x: data.x, y: data.y });
         if(startPos.x+radius>=endPos.x-radius){
-            setStartPosition({x:data.x+radius, y:data.y})
-            console.log("hei")
+            //forsøk på å endre posisjon manuelt når boksene kolliderer, funker ikke
+            // setStartPosition({x:data.x+radius, y:data.y})
+            console.log("Startbox collision")
         }
     }
 
@@ -20,9 +21,21 @@ export default function Dragger(){
         setEndPosition({ x: data.x, y: data.y });
         if(endPos.x-radius<=startPos.x+radius){
             // endPos.x = startPos.x
-            console.log("hallo")
+            console.log("Endbox collision")
         }
     }
+
+
+    //lagrer posisjonene relativt i forhold til lengden av diven og legger
+    // det til som metadata på videoen når man er ferdig å redigere
+
+    // const saveVideo = () => {
+    //     var metadata = {
+    //         "trim_times": this.state.timings,
+    //         "mute": this.state.isMuted
+    //     }
+    //     this.props.saveVideo(metadata)
+    // }
 
 
     return (
@@ -31,6 +44,7 @@ export default function Dragger(){
                 axis="x"
                 onDrag={(e,data) => checkStartCollision(data)}
                 id="start"
+                defaultPosition={{x: 0, y: 0}}
             >
                 <div className="box">Start</div>
             </Draggable>
