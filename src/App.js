1cd6e2bf460a216043e67503d5fd73471b04edb6
Brage <bragmin@hotmail.com>
2022-03-08 11:11:23 +0100

    Removed more useless code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import './App.css';
-import React, { useState } from "react";
+import React from "react";
 import Editor from "./components/Editor";
 
 
 function App() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const trackPos = (data) => {
-    setPosition({ x: data.x, y: data.y });
-  };
-
 
   return (
     <div className="App">
 
-    <div id="wrapper">
-      <Editor
-      axis="x"
-      defaultPosition={{x: 550, y: 400}}
-      onDrag={(e, data) => trackPos(data)}
-      >
-      </Editor>
-    </div>
+      <div id="wrapper">
+        <Editor
+        axis="x"
+        defaultPosition={{x: 550, y: 400}}
+        >
+        </Editor>
+      </div>
 
 
     </div>
