5d0a103a9cd17ca33e2c9d0f840a9de18de1cdec
Brage <brage@apiir.tech>
2022-03-22 13:21:52 +0100

    Moved the trimming code into a function

--- src/python/videoEdit.py
+++ src/python/videoEdit.py
@@ -1,12 +1,15 @@
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
-input_video_path = '../videos/bigBuckBunny.mp4'
-output_video_path = "cutVid.mp4"
+input_video_path = 'src/videos/bigBuckBunny.mp4'
+output_video_path = 'src/videos/trimmedVideo.mp4'
 
 t1 = 3.22
 t2 = 9.03
 
-with VideoFileClip(input_video_path) as video:
-    new = video.subclip(t1, t2)
-    ## may need to change bitrate and/or codec if we change from mp4
-    new.write_videofile(output_video_path, codec="libx264", audio_codec='aac', bitrate="6000k")
+def trim_video(inPath, startTime, endTime, outPath):
+    with VideoFileClip(inPath) as video:
+        new = video.subclip(startTime, endTime)
+        ## may need to change bitrate and/or codec if we change from mp4
+        new.write_videofile(outPath, codec="libx264", audio_codec='aac', bitrate="6000k")
+
+trim_video(input_video_path,t1,t2,output_video_path)
