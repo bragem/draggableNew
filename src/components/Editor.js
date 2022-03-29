b757e3900c72f264a3c858013e4f79f2b1a7fd25
Brage <brage@apiir.tech>
2022-03-29 14:36:33 +0200

    added todo

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -6,6 +6,9 @@ import Myvideo from '../videos/Big_Buck_Bunny_1080_10s_30MB.mp4';
 import ClickableDiv from 'react-clickable-div'
 const radius = 3;
 
+//TODO hele komponenten må være responsiv og pen på mobil, er kun det det skal lages til, ikke web.
+
+
 
 export default function Editor() {
     let maxBound = 640 + radius;
